f8c94a17295ee8b848aeb7e30adfae72acf2d212
John Thompson <john@springframework.guru>
2018-12-09 13:32:19 -0500

    initial project add

---
+++ README.md
@@ -0,0 +1,3 @@
+# Alternate JVM Languages in Maven
+
+This repository has examples of using Maven to compile popular alternate JVM Languages.
---
+++ src/main/java/JavaHelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by jt on 2018-12-09.
+ */
+public class JavaHelloWorld {
+
+    public String getHello(){
+        return "Hello World";
+    }
+
+}
